e6b36543019e62f6b57aac451e58d78b6fb0a99c
Darin Hoover <darinh@gmail.com>
2015-07-18 21:53:22 -0700

    Initial Commit.

---
+++ CodeKata.Tests/Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeKata.Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void Test()
+        {
+        }
+    }
+}
